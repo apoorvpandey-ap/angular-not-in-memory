f5d3d4b3c12b50003254c92b25f307fa9ef8ca2c
rupesh kumar <javax4u@gmail.com>
2020-08-11 17:27:40 +0530

    firebase feature added

--- run.bat
+++ run.bat
@@ -9,5 +9,16 @@ ng serve --host 0.0.0.0 --disable-host-check --ssl true
 #ng generate component signaturepad
 #cmd.exe /c 'run.bat'
 #ng build --prod --base-href=angular
+#below command does not work at run time. many css is not found
+#ng build --prod
 #create war file for jboss
-#jar -cvf angular.war *
+#cd dist/angular-tour-of-heroes/
+#jar -cvf ../../angular.war *
+##firebase configurations
+#npm install -g firebase-tools
+#npm install --save @angular/fire@5.1.1 firebase@5.8.4
+#firebase login
+#firebase init
+##provide this path dist\angular-tour-of-heroes
+#ng build --prod
+#firebase deploy
